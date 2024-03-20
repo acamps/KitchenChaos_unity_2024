0df66c8c1d74ca2f7f7364f6bef09372666b0208
Albert Camps <albert.camps@gmail.com>
2024-03-20 20:38:58 +0100

    Pause, Clear Statics lesson completed
    Interesting to see how to manage memory
    Static listeners need to be removed
    
    Game Input also needs to unsubscribe and be deleted.

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -6,18 +6,35 @@ using UnityEngine.InputSystem;
 
 public class GameInput : MonoBehaviour {
 
+    public static GameInput Instance {get; private set;}
+
     public event EventHandler OnInteractAction;
     public event EventHandler OnInteractAlternateAction;
+    public event EventHandler OnPauseAction;
 
     PlayerInputActions playerInputActions;
 
-    void Awake()
-    {
+    void Awake() {
+        Instance = this;
+
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
 
         playerInputActions.Player.Interact.performed += Interact_performed;
         playerInputActions.Player.InteractAlternate.performed += InteractAlternate_performed;
+        playerInputActions.Player.Pause.performed += Pause_performed;
+    }
+
+    private void OnDestroy() {
+        playerInputActions.Player.Interact.performed -= Interact_performed;
+        playerInputActions.Player.InteractAlternate.performed -= InteractAlternate_performed;
+        playerInputActions.Player.Pause.performed -= Pause_performed;
+
+        playerInputActions.Dispose();     
+    }
+
+    private void Pause_performed(UnityEngine.InputSystem.InputAction.CallbackContext context) {
+        OnPauseAction?.Invoke(this, EventArgs.Empty);
     }
 
     private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext context) {
