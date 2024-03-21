8dbb6101c495b5bd4c2d2788fc953a0304a4b095
Albert Camps <albert.camps@gmail.com>
2024-03-21 07:23:17 +0100

    Lecture about the audio menu
    So far not the funniest lecture :dancer:

---
+++ Assets/Scripts/OptionsUI.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsUI : MonoBehaviour
+{
+    [SerializeField] private Button audioEffectsButton;
+    [SerializeField] private Button musicButton;
+}
