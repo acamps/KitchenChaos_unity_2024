51346150590ad45d3b5b7aa1c65e88571df2e688
Albert Camps <albert.camps@gmail.com>
2024-03-26 07:50:10 +0100

    Added trigger for the animations in code
    The animation code however is within the same class as the UI, which creates a not so nice coupling. However we want to keep it.

--- Assets/Scripts/UI/GameStartCountdownUI.cs
+++ Assets/Scripts/UI/GameStartCountdownUI.cs
@@ -8,7 +8,16 @@ using UnityEngine;
 
 public class GameStartCountdownUI : MonoBehaviour
 {
+    private const string NUMBER_POPUP = "NumberPopup";
+
+    private Animator animator;
+    private int previousCountdownNumber;
     [SerializeField] private TextMeshProUGUI countdownText;
+
+
+    private void Awake() {
+        animator = GetComponent<Animator>(); 
+    }
 
     private void Start() {
         KitchenGameManager.Instance.OnStateChanged += KitchenGameManager_OnStateChanged;
@@ -25,7 +34,14 @@ public class GameStartCountdownUI : MonoBehaviour
     }
 
     private void Update() {
-        countdownText.text = Mathf.Ceil(KitchenGameManager.Instance.GetCountdownToStartTimer()).ToString();
+        int countdownNumber = Mathf.CeilToInt(KitchenGameManager.Instance.GetCountdownToStartTimer());
+        countdownText.text = countdownNumber .ToString();
+
+        if(previousCountdownNumber != countdownNumber) {
+            previousCountdownNumber = countdownNumber;
+            animator.SetTrigger(NUMBER_POPUP);
+            SoundManager.Instance.PlayCountdownSound(); 
+        }
     }
 
     private void Show() {
